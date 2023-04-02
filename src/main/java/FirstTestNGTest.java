76ff27c1b4b0134b04680e5a9411c8da84be65b6
mnaeem <mnaeem@gmail.com>
2023-04-02 21:45:51 +0500

    New login test added with priority

--- src/main/java/FirstTestNGTest.java
+++ src/main/java/FirstTestNGTest.java
@@ -1,7 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,12 +17,29 @@ public class FirstTestNGTest {
         driver.get(baseUrl);
     }
 
-    @Test
+    @Test(priority=1)
     public void verifyHomepageTitle() {
 
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test(priority = 2)
+    public void loginTest() {
+        //Login Page
+        WebElement userNameTextField = driver.findElement(By.name("userName"));
+        userNameTextField.sendKeys("test");
+        WebElement passwordTextField = driver.findElement(By.name("password"));
+        passwordTextField.sendKeys("test");
+        WebElement submitButton = driver.findElement(By.name("submit"));
+        submitButton.click();
+        //Dashboard
+        WebElement successMessageHeading = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3"));
+        String actualSuccessMessageHeadingText = successMessageHeading.getText();
+        String expectedSuccessMessageHeadingText = "Login Successfully";
+        Assert.assertEquals(actualSuccessMessageHeadingText,expectedSuccessMessageHeadingText);
+
     }
 
     @AfterTest
